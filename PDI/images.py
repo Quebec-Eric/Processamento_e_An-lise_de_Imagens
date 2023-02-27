a9e53a5f122ff7403431e3be93ac4d53fd554754
Bryanb27 <bryanb27hadouken@gmail.com>
2023-02-27 17:45:47 -0300

    Alteraçoes em images.py para aumento de dados de treino, por enquanto apenas espelhando

--- PDI/images.py
+++ PDI/images.py
@@ -2,12 +2,20 @@ import imageio.v3 as iio
 import re
 import os 
 from pathlib import Path
+from operator import length_hint 
+from PIL import Image
+import numpy as np
 
 class images():
     def __init__(self, fileFolder):
-        #Criar dois arranjos, um para treino e outro para teste
-        trainList = list()
-        testList = list()
+        #Criar arranjos;
+        #NMNE = Nao Espelhada (Not mirrored) x nao equalizada, NME = Nao espelhada x equalizada
+        #MNE = Espelhada x nao equalizada, ME = Espelhada x equalizada
+        self.trainListNMNE = list()
+        self.trainListNME = list()
+        self.trainListMNE = list()
+        self.trainListME = list()
+        self.testList = list()
     
         #Para os arquivos dentro do path
         y = os.path.join('Images', fileFolder)
@@ -20,7 +28,10 @@ class images():
                 #caso contrario, para lista de teste
                 x = re.findall('[0-9]+', file.name)
                 if (int(x[0]) % 4 == 0):
-                    print(file.name) 
-                    trainList.append(iio.imread(file))
+                    #print(file.name) 
+                    self.trainListNMNE.append(iio.imread(file))
                 else:
-                    testList.append(iio.imread(file))
+                    self.testList.append(iio.imread(file))
+        #Espelhar
+        for i in self.trainListNMNE:
+            self.trainListMNE.append(np.fliplr(i))
