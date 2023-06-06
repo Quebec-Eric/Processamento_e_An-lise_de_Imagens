d70da1c1d17fa9cc3e07b9e2ec978eff164d5a8f
Quebec-Eric <quebec.ericjs@gmail.com>
2023-06-06 15:42:38 -0300

    Resposta

--- PDI/inicio.py
+++ PDI/inicio.py
@@ -6,7 +6,7 @@ from mainwindow import MainWindow
 from Imports import *
 from App.Curso.Logo import Logo
 from App.JanelaCinza.JanelaSeg import SubWindow
-
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
